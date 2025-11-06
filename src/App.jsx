a3de83ebbb6b26227e4dfd560f9325e518cea7c6
Kieran Pritchard <KieranPritchard06@gmail.com>
2025-11-06 23:01:57 +0000

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import ToDoInput from "./components/ToDoInput"
 import TaskList from "./components/TaskList"
+import Header from "./components/Header"
 
 function App(){
   // Stores tasks as a state
@@ -8,6 +9,7 @@ function App(){
 
   return(
     <div>
+      <Header />
       <ToDoInput tasks={tasks} setTasks={setTasks}/>
       <TaskList tasks={tasks} setTasks={setTasks}/>
     </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// Creates header for the app
+
+function Header(){
+    return(
+        <header className="p-4 shadow-md">
+            <h1 className="text-lg">To-Do List</h1>
+        </header>
+    )
+}
+
+export default Header
